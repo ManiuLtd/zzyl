9dcc4b8607b908ce068b4f981520c5fb9dbfdbeb
xhq <i@iyouths.org>
2019-03-16 18:49:21 +0800

    数据库密码

--- hm_ucenter/config/ConnectConfig.php
+++ hm_ucenter/config/ConnectConfig.php
@@ -22,7 +22,7 @@ final class ConnectConfig
         //'host' => '172.18.194.16', //TODO
         'host' => '127.0.0.1',
         'port' => 3306, //TODO
-        'dbname' => 'zhizunyule', //TODO
+        'dbname' => 'old_zhizunyule', //TODO
         'username' => 'root',//TODO
         'password' => 'j24b2qc8qa@#', //TODO
         'charset' => 'utf8',

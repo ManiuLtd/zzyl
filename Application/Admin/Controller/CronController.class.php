fc90b2dee172ecb06043fba1b18d30ddfaf42b5c
xhq <i@iyouths.org>
2019-03-20 23:16:42 +0800

    cs

--- Application/Admin/Controller/CronController.class.php
+++ Application/Admin/Controller/CronController.class.php
@@ -458,7 +458,7 @@ class CronController extends Controller
         \Think\Log::write('######呵呵222#####'.$fontfile);
         $image = new \Think\Image();
         // 在图片右下角添加水印文字 ThinkPHP 并保存为new.jpg
-        $image->open($filepath)->text($recived['userid'],$fontfile,20,'#FF4040',\Think\Image::IMAGE_WATER_SOUTH)->save($filepath);
+        $image->open($filepath)->text($recived['userid'],$fontfile,20,'#FF4040',\Think\Image::IMAGE_WATER_EAST)->save($filepath);
         echo 33;
     }
 

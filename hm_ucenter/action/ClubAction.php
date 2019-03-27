1e9ae6df3e87af699e309633a2e2f5b9fec27c93
xhq <i@iyouths.org>
2019-03-27 20:58:28 +0800

    cs

--- hm_ucenter/action/ClubAction.php
+++ hm_ucenter/action/ClubAction.php
@@ -43,7 +43,7 @@ class ClubAction extends AppAction
      */
     public function friendsGroupList($params)
     {
-        AppModel::returnJson(ErrorConfig::SUCCESS_CODE, ErrorConfig::SUCCESS_MSG_DEFAULT, []);
+        //AppModel::returnJson(ErrorConfig::SUCCESS_CODE, ErrorConfig::SUCCESS_MSG_DEFAULT, []);
         $userID = (int)$params['userID'];
         $result = ClubModel::getInstance()->getFriendsGroupList($userID);
         LogHelper::printDebug($result);

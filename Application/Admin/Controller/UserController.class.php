9cb60e81f780f12d38bd4c8beb1e36165cb7c7fa
xhq <i@iyouths.org>
2019-04-10 18:16:13 +0800

    cs

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -87,6 +87,9 @@ class UserController extends AdminController
         foreach ($dbUserList as $key => $dbUser) {
             $userID = $dbUser['userid'];
             $user = UserModel::getInstance()->getUserInfo($userID);
+            if($userID == 122010){
+                var_dump($user);exit;
+            }
             $user = array_merge($user, $dbUser);
 //            dump($user);
 //            $user['userid'] = $userID;

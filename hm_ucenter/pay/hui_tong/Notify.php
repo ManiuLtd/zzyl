e377d2e0d392e0c6a059beac790776d98ad3adca
xhq <i@iyouths.org>
2019-04-17 18:15:50 +0800

    cs

--- hm_ucenter/pay/hui_tong/Notify.php
+++ hm_ucenter/pay/hui_tong/Notify.php
@@ -48,9 +48,9 @@ class Notify extends AppPay
         $util->writelog ("异步通知参数:" .$util->getURLParam($Params,$merconfig["Url_Param_Connect_Flag"],true,null));
         $this->payLog('-----------------------------huitong----------------:异步通知参数' . $util->getURLParam($Params,$merconfig["Url_Param_Connect_Flag"],true,null));
         //==验签数据
-        $util->writelog ('参数1111'.$Params);
+        $util->writelog ('参数1111'.json_encode($Params));
         $util->writelog ('参数2222'.$merconfig["Url_Param_Connect_Flag"]);
-        $util->writelog ('参数3333'.$removeKeys);
+        $util->writelog ('参数3333'.json_encode($removeKeys));
         $util->writelog ('参数4444'.$merconfig["Md5Key"]);
         if ($util->verifySign($Params, $merconfig["Url_Param_Connect_Flag"], $removeKeys
             , $merconfig["Md5Key"])){
